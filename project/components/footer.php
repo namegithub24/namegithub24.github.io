4318c67a092b23e4f4077d2f2c0855f4114d7ee8
namegithub24 <akungithub212@gmail.com>
2024-07-12 14:31:39 +0700

    hotel

---
+++ project/components/footer.php
@@ -0,0 +1,29 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="#"><i class="fas fa-phone"></i> 08123456</a>
+         <a href="#"><i class="fas fa-phone"></i> 08234567</a>
+         <a href="#"><i class="fas fa-envelope"></i> ghatfhanramadhan@gmail.com</a>
+         <a href="#"><i class="fas fa-envelope"></i> ghatfhanramadhan@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Indonesia, Tambun - 17510</a>
+      </div>
+
+      <div class="box">
+         <a href="#">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">twitter <i class="fab fa-twitter"></i></a>
+         <a href="#">instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="#">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; WEBSITE BOOKING HOTEL BY MORNING PERSON</div>
+
+</section>
+
+<!-- footer section ends -->
---
+++ project/components/user_header.php
@@ -0,0 +1,23 @@
+<!-- header section starts  -->
+
+<section class="header">
+
+   <div class="flex">
+      <a href="#home" class="logo">Hotels And Resorts</a>
+      <a href="#availability" class="btn">Check availability</a>
+      <div id="menu-btn" class="fas fa-bars"></div>
+   </div>
+
+   <nav class="navbar">
+      <a href="index.php#home">HOME</a>
+      <a href="index.php#about">ABOUT</a>
+      <a href="index.php#reservation">RESERVATION</a>
+      <a href="index.php#gallery">GALLERY</a>
+      <a href="index.php#contact">CONTACT</a>
+      <a href="index.php#reviews">REVIEWS</a>
+      <a href="bookings.php">MY BOOKING</a>
+   </nav>
+
+</section>
+
+<!-- header section ends -->
